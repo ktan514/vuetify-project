0dfa7b0842d5a039f73cbe5d395a66b7a2bf933e
katan514 <m5014514m@gmail.com>
2022-05-22 02:44:22 +0900

    tiptap拡張機能追加。 ツールバーのサンプル。

--- src/views/Notes/NoteEditor.vue
+++ src/views/Notes/NoteEditor.vue
@@ -1,26 +1,71 @@
 <template>
-    <editor-content :editor="editor" />
+    <div>
+        <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+            <button
+                :class="{ 'is-active': isActive.bold() }"
+                @click="commands.bold"
+            >
+                Bold
+            </button>
+        </editor-menu-bar>
+        <editor-content :editor="editor" />
+    </div>
 </template>
 
 <script>
 // Import the editor
-import { Editor, EditorContent } from 'tiptap'
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+    Blockquote,
+    CodeBlock,
+    HardBreak,
+    Heading,
+    OrderedList,
+    BulletList,
+    ListItem,
+    TodoItem,
+    TodoList,
+    Bold,
+    Code,
+    Italic,
+    Link,
+    Strike,
+    Underline,
+    History,
+} from 'tiptap-extensions'
 
 export default {
     name: 'NoteEditor',
     components: {
+        EditorMenuBar,
         EditorContent,
     },
     data() {
         return {
-            editor: null,
+            editor: new Editor({
+                extensions: [
+                    new Blockquote(),
+                    new CodeBlock(),
+                    new HardBreak(),
+                    new Heading({ levels: [1, 2, 3] }),
+                    new BulletList(),
+                    new OrderedList(),
+                    new ListItem(),
+                    new TodoItem(),
+                    new TodoList(),
+                    new Bold(),
+                    new Code(),
+                    new Italic(),
+                    new Link(),
+                    new Strike(),
+                    new Underline(),
+                    new History(),
+                ],
+                content: '<p>This is just a boring paragraph</p>',
+            }),
         }
     },
-    mounted() {
-        this.editor = new Editor({
-            content: '<p>This is just a boring paragraph</p>',
-        })
-    },
+    mounted() {},
     beforeDestroy() {
         this.editor.destroy()
     },
