f7daf2f6cbaabdb346c5149da40f8ae521fc6a55
k-takeuchi <m5014514m@gmail.com>
2022-05-26 18:27:14 +0900

    vueを一元化。

--- src/public/components/notes/view-note-editor.vue
+++ src/public/components/notes/view-note-editor.vue
@@ -5,7 +5,17 @@
     </v-container>
 </template>
 
-<script src = "./quill-config.js"></script>
+<script src = "./quill-config.js">
+import quillConfig from './quill-config'
+
+export default {
+    date() {
+        return {
+            content: quillConfig.content,
+        }
+    },
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped src = "./quill-config.css"></style>
+<style scoped src="./quill-config.css"></style>
