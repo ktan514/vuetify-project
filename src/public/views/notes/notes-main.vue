73a3bb869c12a505d7f12e12c2a0b3ae697d7e86
k-takeuchi <m5014514m@gmail.com>
2022-05-27 18:30:02 +0900

    quill-better-tableの導入サンプル。

--- src/public/views/notes/notes-main.vue
+++ src/public/views/notes/notes-main.vue
@@ -2,19 +2,18 @@
     <v-main>
         <v-container>
             <v-layout wrap>
-                <v-text-field
-                    label="Your landing page"
-                    hint="www.example.com/page"
-                    persistent-hint
-                    outlined
-                ></v-text-field>
+                <v-card light class="px-3">
+                    <v-card-title>
+                        <v-text-field
+                            class="headline"
+                            label="Topic's title"
+                            hint="www.example.com/page"
+                            persistent-hint
+                            outlined
+                        ></v-text-field
+                    ></v-card-title>
 
-                <v-card dark class="px-3">
-                    <v-card-title class="headline">トピック編集</v-card-title>
-                    <v-card-subtitle>card subtitle</v-card-subtitle>
-                    <v-divider class="mx-3"></v-divider>
-
-                    <v-spacer class="my-10"></v-spacer>
+                    <v-divider class="ma-3"></v-divider>
 
                     <quillEditor />
 
@@ -32,7 +31,8 @@
 
 <script>
 import routerConfig from '@/router/router-config'
-import quillEditor from '@/components/notes/view-note-editor.vue'
+//import quillEditor from '@/components/notes/view-note-editor.vue'
+import quillEditor from '@/components/notes/view-small-editor.vue'
 
 export default {
     name: routerConfig.siteMap.notes.name,
